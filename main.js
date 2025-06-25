d68a9b14669a96257f123d93a59436ad857b84a3
Kleber Fellipe <kleber.steinke@escola.pr.gov.br>
2025-06-25 10:33:20 -0300

    Terminando o jogo

--- main.js
+++ main.js
@@ -20,27 +20,12 @@ function criarTabuleiro(){
         elemento.style.gridColumnStart = segmento.x;
         elemento.classList.add('snake');
         tabuleiro.appendChild(elemento);
-    } );
+    });
     const elementoComida = document.createElement('div');
     elementoComida.style.gridRowStart = comida.y;
     elementoComida.style.gridColumnStart = comida.x;
     elementoComida.classList.add('food');
     tabuleiro.appendChild(elementoComida);
-
-    function gerarComida(){
-        let novaComida;
-        while (!novaComida || posicaoOcupada(novaComida))
-        {
-            novaComida = {
-            x:Math.floor(Math.random()*18)+2,
-            y:Math.floor(Math.random()*18)+2
-         };
-      }
-      return novaComida;
-    }
-    function posicaoOcupada(posicao){
-        return cobra.some(segmento=> segmento.x === posicao.x && segmento.y===posicao.y);
-    }
 }
     function mudarDirecao(event){
         const tecla = event.key;
@@ -57,6 +42,22 @@ function criarTabuleiro(){
         {
             direcao = 'DIREITA'
         }
+    }
+function mudarDirecao(event){
+    const tecla = event.key;
+    if (tecla === "ArrowUp" && direcao !== 'BAIXO')
+    {
+        direcao = 'CIMA';  
+    } else if(tecla === 'ArrowDown' && direcao !== 'CIMA')
+    {
+        direcao = 'BAIXO';
+    } else if(tecla === 'ArrowLeft' && direcao !== 'DIREITA')
+    {
+        direcao = 'ESQUERDA'
+    } else if(tecla === 'ArrowRight' && direcao !== 'ESQUERDA')
+    {
+        direcao = 'DIREITA'
+    }
 }
 function moverCobra(){
     const cabeca = {...cobra[0]};
@@ -65,7 +66,7 @@ function moverCobra(){
         case 'CIMA':
             cabeca.y--;
             break;
-            case 'BAIXO':
+        case 'BAIXO':
             cabeca.y++;
             break;
         case 'ESQUERDA':
@@ -75,6 +76,7 @@ function moverCobra(){
             cabeca.x++;
             break;
     }
+
     if (verificarColisao(cabeca)) {
         clearInterval(intervalo);
         alert(`Game Over! Pontos: ${pontos}`);
@@ -92,18 +94,34 @@ function moverCobra(){
     }
 
     criarTabuleiro();
+}
 
-    function verificarColisao(cabeca) {
-        if (cabeca.x < 1 || cabeca.x > 20 || cabeca.y < 1 || cabeca.y > 20) {
+function verificarColisao(cabeca) {
+    if (cabeca.x < 1 || cabeca.x > 20 || cabeca.y < 1 || cabeca.y > 20) {
+        return true;
+    }
+    for (let i = 1; i < cobra.length; i++) {
+        if (cobra[i].x === cabeca.x && cobra[i].y === cabeca.y) {
             return true;
-        }
-
-        for (let i = 1; i < cobra.length; i++) {
-            if (cobra[i].x === cabeca.x && cobra[i].y === cabeca.y) {
-                return true;
-            }
         }
     }
     return false;
 }
+
+function gerarComida(){
+    let novaComida;
+    while (!novaComida || posicaoOcupada(novaComida))
+    {
+        novaComida = {
+            x:Math.floor(Math.random()*18)+2,
+            y:Math.floor(Math.random()*18)+2
+        };
+    }
+    return novaComida;
+}
+
+function posicaoOcupada(posicao){
+    return cobra.some(segmento=> segmento.x === posicao.x && segmento.y===posicao.y);
+}
+
 iniciar();
